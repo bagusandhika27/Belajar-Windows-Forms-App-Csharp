10ba33e009555f463f281ea864580000df183888
Bagus Andhika <bagusandhika6@gmail.com>
2024-01-20 13:04:06 +0700

    Membuat Class Koneksi untuk koneksi ke database SQL Server

---
+++ Belajar Inventori/Koneksi.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Belajar_Inventori
+{
+    class Koneksi
+    {
+        public SqlConnection GetConn() 
+        {
+            SqlConnection Conn = new SqlConnection();
+            Conn.ConnectionString = "Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = master; Integrated Security = True; initial catalog = belajarcsharp; integrated security = True; MultipleActiveResultSets = True";
+            return Conn;
+        }
+    }
+}
