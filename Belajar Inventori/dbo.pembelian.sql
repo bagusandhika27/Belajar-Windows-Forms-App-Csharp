a5fbf627fcdfbd83efcfc3bd82b9d5a68c466a50
Bagus Andhika <bagusandhika6@gmail.com>
2024-01-23 11:04:07 +0700

    Update query SQL

---
+++ Belajar Inventori/dbo.pembelian.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[pembelian] (
+    [kode]       VARCHAR (10) NOT NULL,
+    [kodebarang] VARCHAR (10) NOT NULL,
+    [jumlah]     INT          NOT NULL,
+    PRIMARY KEY CLUSTERED ([kode] ASC)
+);
+
